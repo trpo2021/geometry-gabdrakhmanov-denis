3d895b1e9eae60200331784e9e080e5ff1939caa
gabdrakhmanov-denis <gabdrakhmanov-denis@github.com>
2021-05-11 18:25:23 +0700

    main.cpp was edited

--- src/geometry/main.cpp
+++ src/geometry/main.cpp
@@ -1,39 +1,48 @@
-#include "lib_circle.h"
-#include <cstring>
 #include <iostream>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include "circle_compute.h"
+#include "wkt_check.h"
+
 using namespace std;
 
 int main()
 {
     cout << "\x1B[2J\x1B[H";
-    char figur[25];
+    char _char[25];
     cout << "Enter figure: ";
-    scanf("%[^\n]s", figur);
-    int length = strlen(figur);
+    scanf("%[^\n]s", _char);
+    int length = strlen(_char);
     string str = "";
-    for (int i = 0; i < length; i++)
-        str += figur[i];
-    int flag = wkt_check(str);
-    if (flag == 1) {
-        Circle coordinates;
-        coordinates.x = atof(str.substr(str.find_first_of("(") + 1,
-                                        str.find_first_of(' ') - 1)
-                                     .c_str());
-        coordinates.x = atof(str.substr(str.find_first_of(' ') + 1,
-                                        str.find_first_of(",") - 1)
-                                     .c_str());
-        coordinates.r = atof(str.substr(str.find_first_of(",") + 1,
-                                        str.find_last_of(")") - 1)
-                                     .c_str());
-        Calculations result;
-        result = circle_compute(coordinates.r);
+    for (int i = 0; i < length; i++) {
+        str += _char[i];
+    }
+
+    int check_flag = wkt_check(str);
+    if (check_flag == 1) {
+        Circle tokens;
+
+        tokens.x = atof(str.substr(str.find_first_of("(") + 1,
+                                   str.find_first_of(' ') - 1)
+                                .c_str());
+        tokens.x = atof(str.substr(str.find_first_of(' ') + 1,
+                                   str.find_first_of(",") - 1)
+                                .c_str());
+        tokens.r = atof(str.substr(str.find_first_of(",") + 1,
+                                   str.find_last_of(")") - 1)
+                                .c_str());
+
+        Calculations result_calc;
+        result_calc = circle_compute(tokens.r);
+
         cout << str << endl;
-        cout << "P = " << result.perimeter << endl;
-        cout << "S = " << result.area << endl;
-    } else
-        cout << endl << "Error: wkt-format violated" << endl;
+        cout << "P = " << result_calc.perimeter << endl;
+        cout << "S = " << result_calc.area << endl;
+    } else {
+        cout << "\nError: wkt-format violated" << endl;
+    }
     return 0;
 }
